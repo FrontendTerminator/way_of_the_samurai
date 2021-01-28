5bcad7a8b523b57bf0a351fe828913747f0e93f9
FrontendTerminator <mikutishvili.koba@gmail.com>
2021-01-29 00:14:30 +0300

    Profile and Dialogs reducers is refactored

--- src/redux/Profile-reducer.tsx
+++ src/redux/Profile-reducer.tsx
@@ -29,29 +29,26 @@ const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT"
 const profileReducer = (state: ProfilePageType = initialState, action: ProfileReducerActionType) => {
     switch (action.type) {
         case ADD_POST: {
-
             let newPost = {
                 id: 5,
                 message: state.newPostText,
                 likesCount: 0
             }
-
             return {
                 ...state,
-                posts: [...state.posts, {newPost}],
+                posts: [...state.posts, newPost],
                 newPostText: ""
             }
-
-            let newPost = {
-                id: 5,
-                message: state.newPostText,
-                likesCount: 0
-            }
-            let stateCopy = {...state}
-            stateCopy.posts = [...state.posts]
-            stateCopy.posts.push(newPost)
-            stateCopy.newPostText = ""
-            return stateCopy
+            // let newPost = {
+            //     id: 5,
+            //     message: state.newPostText,
+            //     likesCount: 0
+            // }
+            // let stateCopy = {...state}
+            // stateCopy.posts = [...state.posts]
+            // stateCopy.posts.push(newPost)
+            // stateCopy.newPostText = ""
+            // return stateCopy
         }
         case UPDATE_NEW_POST_TEXT: {
             return {
