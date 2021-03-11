eaea9805568bebe06f39ff3070b0ac1eaf22bfaf
FrontendTerminator <mikutishvili.koba@gmail.com>
2021-03-11 16:48:42 +0300

    71. setState (local state). Status is added

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
@@ -2,6 +2,8 @@ import React, {useEffect} from "react";
 import s from "./ProfileInfo.module.css"
 import {ProfileType} from "../../../../redux/Profile-reducer";
 import {Preloader} from "../../../Common/Preloader/Preloader";
+import {ProfileStatus} from "./ProfileStatus";
+
 
 type ProfileInfoType = {
     profile: ProfileType | null
@@ -15,12 +17,12 @@ const ProfileInfo: React.FC<ProfileInfoType> = (props) => {
 
     return (
         <div>
-            <div>
+            {/*<div>
                 <img src="https://lh5.googleusercontent.com/oDUgUwudwBlIQ3WSyBE3gZ58_tqhKTDBkic65snFp2x5ZKamuzmyfG-WqYI8AC5vl1iu4RAZhW7JdwnCdyW0lA5RNyCbA5XjW6dBVcHw1hPbYZ1yGX82YIH2pWi4JdgFY38VPPd4"/>
-            </div>
+            </div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large}/>
-                ava
+                <ProfileStatus status={"Hello my friends"}/>
             </div>
             <div>
                 Обо мне: {props.profile.aboutMe}
